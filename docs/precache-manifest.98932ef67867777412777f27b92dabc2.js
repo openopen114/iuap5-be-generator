b0c8334e5ec474829e1b497611e6396b0ae1189f
openopen <openopen114@gmail.com>
2020-03-26 21:20:27 +0800

    test docs

---
+++ docs/precache-manifest.98932ef67867777412777f27b92dabc2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "688b5546eaa9dc6f1344e3b3ca6b4d15",
+    "url": "/iuap5-be-generator/index.html"
+  },
+  {
+    "revision": "c966a12267e378fa5d1e",
+    "url": "/iuap5-be-generator/static/css/2.bc4a2552.chunk.css"
+  },
+  {
+    "revision": "6826774a25bd133045ec",
+    "url": "/iuap5-be-generator/static/css/main.9d141514.chunk.css"
+  },
+  {
+    "revision": "c966a12267e378fa5d1e",
+    "url": "/iuap5-be-generator/static/js/2.713177d8.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/iuap5-be-generator/static/js/2.713177d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6826774a25bd133045ec",
+    "url": "/iuap5-be-generator/static/js/main.843fda73.chunk.js"
+  },
+  {
+    "revision": "01ff7c7abee3afb5f8ae",
+    "url": "/iuap5-be-generator/static/js/runtime-main.34e4b507.js"
+  }
+]);
