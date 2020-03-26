1b21d245cf639b1556dfde2a46b65d3ff12a9395
openopen <openopen114@gmail.com>
2020-03-26 21:08:47 +0800

    test

--- src/components/HighlightArea/HighlightArea.jsx
+++ src/components/HighlightArea/HighlightArea.jsx
@@ -1,22 +1,13 @@
 import React, { useContext } from "react";
 import AppContext from "../../Context/AppContext";
-<<<<<<< HEAD
 
 import Highlight from "react-highlight";
-=======
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { xonokai } from "react-syntax-highlighter/dist/esm/styles/prism";
-
->>>>>>> 6436712a904182daf60ed735ab2be80f5e3b86fd
 import { CopyToClipboard } from "react-copy-to-clipboard";
 
 import { Button } from "antd";
 import { CopyOutlined } from "@ant-design/icons";
 
-<<<<<<< HEAD
 import moduleName from "../../../node_modules/highlight.js/scss/dracula.scss";
-=======
->>>>>>> 6436712a904182daf60ed735ab2be80f5e3b86fd
 import "./HighlightArea.scss";
 
 export default function HighlightArea() {
@@ -28,11 +19,7 @@ export default function HighlightArea() {
         return (
           <div key={`${item.title}`}>
             <h1 className="result-title">
-<<<<<<< HEAD
               [{`${item.location}`}] {`${item.title}`}
-=======
-              [{`${item.location}`}]{`${item.title}`}
->>>>>>> 6436712a904182daf60ed735ab2be80f5e3b86fd
             </h1>
 
             <CopyToClipboard text={`${item.codeString}`}>
@@ -41,17 +28,11 @@ export default function HighlightArea() {
               </Button>
             </CopyToClipboard>
 
-<<<<<<< HEAD
             <Highlight className="java">{`${item.codeString}`}</Highlight>
 
             {/* <SyntaxHighlighter language="java" style={xonokai}>
               {`${item.codeString}`}
             </SyntaxHighlighter> */}
-=======
-            <SyntaxHighlighter language="java" style={xonokai}>
-              {`${item.codeString}`}
-            </SyntaxHighlighter>
->>>>>>> 6436712a904182daf60ed735ab2be80f5e3b86fd
           </div>
         );
       })}
